8525d1e0a2c4a7ed0a4cfc910ee2f52f52625194
zombie <re2=cool!>
2014-01-20 09:45:16 +0100

    - added http-server task in gruntfile - added grunt-http-server as dev dependency in package.json

--- src/sentiment/Gruntfile.js
+++ src/sentiment/Gruntfile.js
@@ -17,13 +17,26 @@ module.exports = function(grunt) {
         },
         concurrent : {
             run : {
-                tasks : ['execute:jobs', 'execute:tasks', 'execute:scrapper'],
+                tasks : ['execute:jobs', 'execute:tasks', 'execute:scrapper', 'http-server:adminsrv'],
                 options : { logConcurrentOutput: true }
+            }
+        },
+        'http-server' : {
+            adminsrv : {
+                root : './website',
+                port : 9123,
+                host : '127.0.0.1',
+                showDir : true,
+                autoIndex : true,
+                defaultExt : 'html',
+                runInBackground : false
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-execute');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-http-server');
+
     grunt.registerTask('default', ['concurrent:run']);
 };
